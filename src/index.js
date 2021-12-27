863eff647769726bd727b955e397c18d3472c9b7
googray <shat.rom07@gmail.com>
2021-12-27 21:54:17 +0100

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let units = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let dozens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    function word2Didg(number) {
+        const checker = number % 10;
+        const interim = Math.floor(number / 10);
+        return dozens[interim] + " " + toReadable(checker);
+    }
+    function word3Didg(number) {
+        const checker = number % 100;
+        const interim = Math.floor(number / 100);
+        return checker === 0
+            ? units[interim] + " hundred " + ""
+            : units[interim] + " hundred " + word2Didg(checker);
+    }
+
+    switch (("" + number).length) {
+        case 1:
+            return units[number];
+        case 2:
+            return word2Didg(number);
+        case 3:
+            return word3Didg(number);
+        default:
+            return null;
+    }
+};
